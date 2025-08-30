62e78965b0fc45cb43636cdcad6d63bb57a63591
Bharath <bharathsde05@gmail.com>
2025-08-30 11:57:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,23 @@ import path from "path";
 const app = express();
 
 const server = http.createServer(app);
+const url=`https://collab-code-editer.onrender.com`;
+
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 
 const io = new Server(server, {
   cors: {
